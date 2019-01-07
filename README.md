a7108a24cf76834e7574dda654ab96351d7a3241
watanabeyu <fight.ippatu5648@gmail.com>
2019-01-07 11:40:18 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# expo-qr-notify
+This repository inspired by https://github.com/expo/expo-postpublish-slack-notify.  
+Post a notification on Slack and Github PR comment whenever your project is published.  
+Presumably you already know this is happening so it's probably more useful to non-you people, such as your team or your grandparents.
+
+## What is the difference with [expo-postpublish-slack-notify](https://github.com/expo/expo-postpublish-slack-notify)?
+* If you set github token, it comment to github pull request by triggerd to circleci.
+* Post a notification on Slack not only text but also QR image and url and pull request url.
+
+## Installation
+1. [Create an incoming webhook](https://api.slack.com/incoming-webhooks) and keep the window open with the URL.
+2. `npm install --save-dev expo-qr-notify` in your project.
+3. Setting a circleci project, and check on **Only build pull requests** (Settings >> Advanced Settings).
+4. Add the following to your app.json within the "expo" key, or set a circleci environment variables.
+
+### Configuration variables
+|   |on app.json|on circleci|
+|---|-----------|-----------|
+|slack incoming url|`slack_webhook`|`SLACK_WEBHOOK`|
+|notification channel|`slack_channel`|`SLACK_CHANNEL`|
+|github user token|`git_service_token`|`GIT_SERVICE_TOKEN`|
+
+#### app.json
+```json
+"hooks": {
+  "postPublish": [
+    {
+      "file": "expo-qr-notify",
+      "config": {
+        "slack_webhook": "enter_your_slack_incoming_webhook_url",
+        "slack_channel": "#general",
+        "git_service_token": "enter_your_github_token"
+      }
+    }
+  ]
+}
+```
+
+#### circleci
+![on circleci](./extras/variables.png "on circle ci")
+
+## Example
+[Check out example](./example).  
+This is a simple example.  
+If you run circleci, set below variables on [.circleci/config.yml](./example/.circleci/config.yml) or circleci environment variables.
+* `EXPO_ACCOUNT`
+* `EXPO_PASSWORD`
---
+++ index.js
@@ -0,0 +1,78 @@
+const fetch = require('node-fetch');
+
+module.exports = async ({ url, iosManifest, config }) => {
+  const {
+    iconUrl,
+    version,
+    releaseChannel,
+    name,
+    primaryColor,
+  } = iosManifest;
+
+  const publishURL = releaseChannel === 'default' ? url : `${url}?release-channel=${encodeURIComponent(releaseChannel)}`;
+  const qrURL = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${publishURL}`;
+
+  /* github */
+  const gitServiceToken = process.env.GIT_SERVICE_TOKEN || config.git_service_token;
+  const prNumber = process.env.CIRCLE_PULL_REQUEST ? process.env.CIRCLE_PULL_REQUEST.split('/').pop(-1) : null;
+  let htmlURL = null;
+
+  if (gitServiceToken) {
+    if (prNumber) {
+      try {
+        const response = await fetch(`https://api.github.com/repos/${process.env.CIRCLE_PROJECT_USERNAME}/${process.env.CIRCLE_PROJECT_REPONAME}/issues/${prNumber}/comments`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `token ${gitServiceToken}`,
+          },
+          body: JSON.stringify({
+            body: `v${version} ${publishURL}\n\n![QRURL](${qrURL})`,
+          }),
+        }).then(res => res.json());
+
+        htmlURL = response.html_url;
+      } catch (e) {
+        console.log('error:', e);
+      }
+    }
+  } else {
+    console.log('process.env.GITHUB_TOKEN || app.json.hooks.postpublish[].config.github_token not set');
+  }
+
+  /* slack */
+  const slack = {
+    webhook: process.env.SLACK_WEBHOOK || config.slack_webhook,
+    channel: process.env.SLACK_CHANNEL || config.slack_channel,
+  };
+
+  if (slack.webhook && slack.channel) {
+    try {
+      await fetch(slack.webhook, {
+        method: 'post',
+        body: JSON.stringify({
+          icon_url: iconUrl,
+          unfurl_links: 0,
+          username: name,
+          channel: slack.channel || '',
+          attachments: [{
+            color: primaryColor || '',
+            title: publishURL,
+            title_link: publishURL,
+            image_url: qrURL,
+            fields: [{
+              title: `v${version}`,
+            }],
+            footer: (prNumber && htmlURL) && htmlURL,
+            ts: (prNumber && htmlURL) && (new Date().getTime() / 1000),
+          }],
+        }),
+      });
+    } catch (e) {
+      console.log('error:', e);
+    }
+  } else {
+    console.log('process.env.SLACK_WEBHOOK || app.json.hooks.postpublish[].config.slack_webhook not set');
+    console.log('process.env.SLACK_CHANNEL || app.json.hooks.postpublish[].config.slack_channel not set');
+  }
+};
